168cf5f67889e9acac6ebc293092ddf4d657c7ef
Luiz Guilherme <jobs.lugui@gmail.com>
2024-07-15 16:41:43 -0300

    feat: more validations tests

--- e2e/makeLogin.spec.ts
+++ e2e/makeLogin.spec.ts
@@ -6,5 +6,34 @@ test.describe("Login page", () => {
         const loginPage = new LoginPage(page);
         
         await loginPage.visit();
+        await loginPage.doLogin('teste@teste.com', '123');
+        await loginPage.loginSuccess();
     })
+    test("Shouldn't do login with invalid e-mail", async ({page}) => {
+        const loginPage = new LoginPage(page);
+            
+        await loginPage.visit();
+        await loginPage.doLogin('erradoteste@teste.com', '123');
+        await loginPage.showingErrorMessage('Você não está autorizado a acessar este recurso');
+    })
+    
+    test("Shouldn't do login with incorrect format e-mail", async ({page}) => {
+        const loginPage = new LoginPage(page);
+        
+        await loginPage.visit();
+        await loginPage.checkEmail('testeteste.com')
+        await loginPage.showingErrorMessage("E-mail inválido")
+
+    })
+    test("Shouldn't do login with empty fields", async ({page}) => {
+        const loginPage = new LoginPage(page);
+        
+        await loginPage.visit();
+        await loginPage.checkEmptyFields('', '');
+        await loginPage.showingErrorMessage("E-mail é obrigatório");
+        await loginPage.showingErrorMessage("Senha é obrigatória");
+
+
+    })
+
 })
--- e2e/page-objects/LoginPage.ts
+++ e2e/page-objects/LoginPage.ts
@@ -1,13 +1,52 @@
-import { Page } from "@playwright/test";
+import { Locator, Page, expect } from "@playwright/test";
 
 export default class LoginPage {
     private readonly page: Page;
+    private readonly btnLogin: Locator;
+    private readonly inputPass: Locator;
+    private readonly inputEmail: Locator;
+    private readonly btnAccessAccount: Locator;
 
     constructor(page: Page){
         this.page = page
+        this.btnLogin = page.getByTestId('botao-login');
+        this.inputEmail = page.getByTestId('input-email');
+        this.inputPass = page.getByTestId('input-senha');
+        this.btnAccessAccount = page.getByTestId('botao-acessar-conta');
     }
+
+
     async visit(){
         await this.page.goto("/");
+        await this.btnLogin.click();
+        await expect(this.page).toHaveURL("/auth/login");
+    }
+
+    async doLogin(email: string, pass: string){
+        await this.inputEmail.fill(email);
+        await this.inputPass.fill(pass);
+        await this.btnAccessAccount.click();
+    }
+
+    async checkEmail(email: string){
+        await this.inputEmail.fill(email);
+        await this.inputEmail.blur();
+    }
+
+    async checkEmptyFields(email: string, pass: string){
+        await this.inputEmail.fill(email);
+        await this.inputEmail.blur();
+        await this.inputPass.fill(pass);
+        await this.inputPass.blur();
+    }
+
+    async loginSuccess(){
+        await expect(this.page).toHaveURL('/home');
+    }
+
+    async showingErrorMessage(message: string){
+        const errorEl = this.page.getByText(message);
+        await expect(errorEl).toBeVisible();
     }
 
 }
